e1bbf759f04974bab7f54cccc11748e0394d7f1c
Storius <hieupt1003@gmail.com>
2026-03-11 15:36:23 +0700

    chuẩn bị services, models, locales cho giao diện

--- src/locales/en-US/menu.ts
+++ src/locales/en-US/menu.ts
@@ -10,4 +10,15 @@ export default {
 	'menu.DanhMuc': 'Danh mục',
 	'menu.DanhMuc.ChucVu': 'Chức vụ',
 	'menu.DanhMuc.LoaiPhongBan': 'Loại phòng ban',
+
+	'menu.DatLichDichVu': 'Service Booking',
+	'menu.DatLichDichVu.QuanLy': 'Administration',
+	'menu.DatLichDichVu.QuanLy.NhanVien': 'Staff',
+	'menu.DatLichDichVu.QuanLy.DichVu': 'Services',
+	'menu.DatLichDichVu.QuanLy.LichHen': 'Manage Appointments',
+	'menu.DatLichDichVu.LichHen': 'Appointments',
+	'menu.DatLichDichVu.LichHen.DanhSach': 'Appointment List',
+	'menu.DatLichDichVu.LichHen.DatLich': 'New Appointment',
+	'menu.DatLichDichVu.DanhGia': 'Reviews',
+	'menu.DatLichDichVu.BaoCao': 'Reports',
 };
