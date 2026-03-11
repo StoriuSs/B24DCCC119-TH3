b878017fabc3caf5d4967f9f6631de32cc5c6d4a
Storius <hieupt1003@gmail.com>
2026-03-11 12:51:39 +0700

    init

---
+++ src/locales/en-US/menu.ts
@@ -0,0 +1,13 @@
+export default {
+	'menu.login': 'Đăng nhập',
+	'menu.account': 'Tài khoản',
+	'menu.account.center': 'Cá nhân',
+	'menu.Dashboard': 'Trang chủ',
+	'menu.News': 'Tin tức',
+	'menu.GuessNumber': 'Guess Number Game',
+	'menu.TodoList': 'Todo List',
+
+	'menu.DanhMuc': 'Danh mục',
+	'menu.DanhMuc.ChucVu': 'Chức vụ',
+	'menu.DanhMuc.LoaiPhongBan': 'Loại phòng ban',
+};
---
+++ src/locales/vi-VN/menu.ts
@@ -0,0 +1,13 @@
+export default {
+	'menu.login': 'Đăng nhập',
+	'menu.account': 'Tài khoản',
+	'menu.account.center': 'Cá nhân',
+	'menu.Dashboard': 'Trang chủ',
+	'menu.About': `Giới thiệu ${APP_CONFIG_TIEN_TO_TRUONG.toLowerCase()}`,
+	'menu.News': 'Tin tức',
+	'menu.GuessNumber': 'Game Đoán Số',
+	'menu.TodoList': 'Danh sách công việc',
+
+	'menu.DanhMuc': 'Danh mục',
+	'menu.DanhMuc.ChucVu': 'Chức vụ',
+};
